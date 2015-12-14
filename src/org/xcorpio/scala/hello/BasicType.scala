771f3486f26c868a5652b58cbaa166b0c0ccbf9f
xcorpio <'xcorpio@qq.com'>
2015-12-14 23:41:26 +0800

    add basic type opts

--- src/org/xcorpio/scala/hello/BasicType.scala
+++ src/org/xcorpio/scala/hello/BasicType.scala
@@ -5,5 +5,9 @@ object BasicType {
     println(-3.4.abs)
     println(1 max 5)
     println(4 to 6)
+    println((1).+(3))
+    val s = "Hello, World"
+    println(s indexOf 'o')
+    println(2.0.unary_-)
   }
 }
