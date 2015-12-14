08b82ca1422285ee4ea81c8bb3e92988f7104f2d
xcorpio <'xcorpio@qq.com'>
2015-12-15 00:11:37 +0800

    update scala class

--- src/org/xcorpio/scala/hello/Rational.scala
+++ src/org/xcorpio/scala/hello/Rational.scala
@@ -1,5 +1,14 @@
 package org.xcorpio.scala.hello
 
 class Rational(n: Int, d: Int) {
+  require(d!=0)
   println("Created " + n + "/" + d)
+  
+  override def toString = n + "/" + d
+}
+object Rational {
+  def main(args: Array[String]): Unit = {
+    val r = new Rational(1, 0)
+    println(r)
+  }
 }
