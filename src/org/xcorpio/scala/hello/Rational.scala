ee601b02a8ac4c8803da756ad7e31010bdd420c8
Xcorpio <xcorpio@qq.com>
2015-12-16 10:51:44 +0800

    implicit usage

--- src/org/xcorpio/scala/hello/Rational.scala
+++ src/org/xcorpio/scala/hello/Rational.scala
@@ -10,6 +10,9 @@ class Rational(n: Int, d: Int) {
   def +(that: Rational) = 
     new Rational(numerator * that.denominator + that.numerator * denominator, denominator * that.denominator)
   
+  def + (i: Int) = 
+    new Rational(numerator + i * denominator, denominator)
+  
   def *(that: Rational) = 
     new Rational(numerator * that.numerator, denominator * that.denominator)
   
@@ -21,10 +24,17 @@ class Rational(n: Int, d: Int) {
     if(b==0) a else gcd(b, a % b)
 }
 object Rational {
+  
+  // 隐式转换要在同一作用域
+  implicit def intToRational(x: Int) = 
+    new Rational(x)
+  
   def main(args: Array[String]): Unit = {
     val x = new Rational(1, 2)
     val y = new Rational(2, 3)
     println(x + y)
-    println( x + x * y)
+    println(x + x * y)
+    println(x + 1)
+    println(1 + y)
   }
 }
