69efb08a44020de3923bc178f2270589837bcf46
Xcorpio <xcorpio@qq.com>
2016-01-08 13:35:29 +0800

    change package structure

--- src/org/xcorpio/scala/hellopackage/PackageUse.scala
+++ src/org/xcorpio/scala/hellopackage/PackageUse.scala
@@ -1,4 +1,4 @@
-package org.xcorpio.scala.hellotrait {
+package org.xcorpio.scala.hellopackage {
 
   class PackageUse {
 
@@ -7,7 +7,7 @@ package org.xcorpio.scala.hellotrait {
 
 package HH {
   class Hehe() {
-    
+
   }
 }
 
@@ -15,34 +15,34 @@ package launch {
   class Booster3
 }
 package rockets {
-  
+
   package navigation {
-    
+
     class Navigation {
       val map = new StartMap
     }
-    
+
     class StartMap
-    
+
     package launch {
       class Booster1
     }
   }
-  
+
   package launch {
     class Booster2
   }
-  
+
   class MissionControl {
     val booster1 = new navigation.launch.Booster1
     val booster2 = new launch.Booster2
     val booster3 = new _root_.launch.Booster3
   }
-  
+
   class Ship {
     val nav = new navigation.Navigation
   }
-  
+
   class Fleets {
     class Fleet {
       def addShip() = new Ship
