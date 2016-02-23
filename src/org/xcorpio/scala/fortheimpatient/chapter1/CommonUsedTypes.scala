ecebb957f0dd7528379f92afe0b0b232d901ebef
xcorpio <xcorpio@qq.com>
2016-02-23 12:38:50 +0800

    scala for the impatient chapter 1

---
+++ src/org/xcorpio/scala/fortheimpatient/chapter1/CommonUsedTypes.scala
@@ -0,0 +1,17 @@
+package org.xcorpio.scala.fortheimpatient.chapter1
+
+import scala.collection.immutable.Range
+
+object CommonUsedTypes {
+  def main(args: Array[String]): Unit = {
+    val res = "hello".intersect("lo")
+    println(res)
+    println(99.6.toDouble)
+    println(44.22.toDegrees)
+    println(1.to(10))
+    println(1 until 10)
+    
+    val range1 = new Range(1, 10, 2)
+    println(range1)
+  }
+}
---
+++ src/org/xcorpio/scala/fortheimpatient/chapter1/FunctionsAndMethods.scala
@@ -0,0 +1,12 @@
+package org.xcorpio.scala.fortheimpatient.chapter1
+
+object FunctionsAndMethods {
+  def main(args: Array[String]): Unit = {
+    println(math.Pi)
+    println(scala.math.Pi)
+    import scala.math._
+    println(sqrt(2))
+    println(pow(2, 4))
+    println(min(4,5))
+  }
+}
